6daa9bc7375384e6f6510e7e8a1152a501473ea1
Kyle1413 <kylet1412@gmail.com>
2020-04-27 20:53:07 -0400

    Create tags.php

---
+++ tags.php
@@ -0,0 +1,61 @@
+<html>
+<head>
+  <style>
+  <?php include 'style.css'; 
+  session_start();
+
+  ?>
+  </style>
+</head>
+
+<body>
+    <center>
+<?php 
+include "Database/helpers.php";
+
+  if(!isset($_SESSION["loggedin"]) || ($_SESSION["loggedin"] !== true))
+  {
+    
+
+  }
+
+  if(isset($_GET["currentTag"]))
+  {
+
+      $currentTag = $_GET["currentTag"];
+      echo "<h2 class='headerText'>", $currentTag, " Posts", "</h2>";
+      $posts = tag_posts($currentTag);
+      if($posts != null)
+      {
+        foreach($posts as $post)
+        {
+            echo "<div class=\"post\">", "<img src=\"", $post["file_location"], "\" height =\"50\" width=\"50\"></img>", "<p class='subText'>", $post['postID'], " | ",
+             $post['dateTime'], "</p>", "</div>";
+        }
+
+      }
+  }
+  else
+  {
+    echo "<h2 class='headerText'> Tags </h2>";
+      $taglist = tag_list();
+    //"tag_name" "description" "post_count"
+      foreach($taglist as $tag)
+      {
+        echo "<div class=\"tag\">", "<a id=\"clickHere\" href=\"?currentTag=", $tag["tag_name"], "\">", $tag["tag_name"], " | Posts: ", $tag["post_count"], "</a>",
+          "<p class='subText'>", $tag["description"], "</p></div>";
+
+      }
+  }
+
+
+
+
+?>
+
+</center>
+
+
+
+</body>
+</html>
