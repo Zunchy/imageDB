5c8d908bedfb986ab3c8cafd65ebed27c135da09
Kyle1413 <kylet1412@gmail.com>
2020-04-27 22:13:03 -0400

    Update postVerify.php

--- postVerify.php
+++ postVerify.php
@@ -1,29 +1,48 @@
 <?php
 include "Database/dbGlobals.php";
 include "Database/helpers.php";
+session_start();
+if(!isset($_SESSION["loggedin"]) || ($_SESSION["loggedin"] !== true))
+ exit;
 
-// Needs later include account verification
+$user = $_SESSION['user'];
+
+global $dbservername, $dbusername, $dbpassword, $dbname;
+
+$conn = mysqli_connect($dbservername, $dbusername, $dbpassword, $dbname);
+
+if(! $conn )
+    die('Could not connect: ' . mysqli_error($connect));
+
+
+  $sql = $conn->prepare("SELECT userID FROM users WHERE userName = ? limit 1");
+  $sql->bind_param("s", $user);
+
+  if (!$sql->execute()) {
+    trigger_error('Invalid query: ' . $conn->error);
+}
+$result = $sql->get_result();
+$userid = $result->fetch_assoc()["userID"];
+
 
 $Tag = $_POST['tag'];
 
 $ImageName = $_FILES['img']['name'];
 $ImageTMP = $_FILES['img']['tmp_name'];
+$datetime = date("m-d-Y_hia");
 
-$random = rand(1, 999);
-
+$filepath = "images/$datetime.$ImageName";
 $trialuser = 3;
-// User ID hard set to 3, for testing purposes.
 
 if ($Tag == true)
 {
-add_post_with_tag($trialuser, $ImageTMP, $Tag);
+add_post_with_tag($userid, $filepath, $Tag);
 }
 
 else
 {
-add_post_no_tag($trialuser, $ImageTMP);
+add_post_no_tag($userid, $filepath);
 }
 
-move_uploaded_file($ImageTMP, "images/$random.$ImageName");
+move_uploaded_file($ImageTMP, $filepath);
 header("Location: home.php");
-?>
